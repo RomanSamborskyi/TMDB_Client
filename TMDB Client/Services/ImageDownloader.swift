006c550cc33fa94f9b6d7a3dbe9c7cd16ed9a3a7
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2024-07-31 09:16:36 +0200

    Added: Image cache

--- TMDB Client/Services/ImageDownloader.swift
+++ TMDB Client/Services/ImageDownloader.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class ImageDownloader {
     
+    private lazy var cache = CacheManager.instance
+    
     func fetchImage(with session: URLSession, request: URLRequest) async throws -> UIImage? {
         
-        let (data, response) = try await session.data(for: request)
-        let returnedData = try sessionHandler(data: data, response: response)
-        
-        return UIImage(data: returnedData)
+        if let image = cache.getImage(for: request.url?.description ?? "") {
+            print("Image from cache")
+            return image
+        } else {
+            let (data, response) = try await session.data(for: request)
+            let returnedData = try sessionHandler(data: data, response: response)
+            
+            guard let image = UIImage(data: returnedData) else {
+                throw AppError.invalidData
+            }
+            cache.add(image: image, with: request.url?.description ?? "")
+            print("New image")
+            return image
+        }
     }
     
     private func sessionHandler(data: Data?, response: URLResponse) throws -> Data {
