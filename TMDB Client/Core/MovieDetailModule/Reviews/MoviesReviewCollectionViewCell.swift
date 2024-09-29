98ce60b36b63114ac1fcae105cd581ba16cbce92
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2024-09-29 13:54:03 +0200

    added review view and collection cell files

---
+++ TMDB Client/Core/MovieDetailModule/Reviews/MoviesReviewCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesReviewCollectionViewCell.swift
+//  TMDB Client
+//
+//  Created by Roman Samborskyi on 29.09.2024.
+//
+
+import UIKit
+
+class MoviesReviewCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ TMDB Client/Core/MovieDetailModule/Reviews/MoviesReviewView.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesReviewView.swift
+//  TMDB Client
+//
+//  Created by Roman Samborskyi on 29.09.2024.
+//
+
+import UIKit
+
+class MoviesReviewView: UIView {
+    
+}
