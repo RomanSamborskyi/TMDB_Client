83084d0391002765232816dc6a0c680af612b4ce
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2024-11-10 09:45:42 +0100

    made some improvements in login interactor tests

--- TMDB Client_Tests/LoginInteractor_Tests.swift
+++ TMDB Client_Tests/LoginInteractor_Tests.swift
@@ -9,55 +9,50 @@ import XCTest
 @testable import TMDB_Client
 
 final class LoginInteractor_Tests: XCTestCase {
+    
+    var loginInteractor: LoginInteractorProtocol?
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        loginInteractor = LoginInteractor()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        loginInteractor = nil
     }
     func test_LoginInteractor_createNewSession_shouldBeNil() async throws {
         
-        let login = LoginInteractor()
-        
-        XCTAssertNil(login.newSession)
+        XCTAssertNil(loginInteractor?.newSession)
        
     }
     func test_LoginInteractor_createNewSession_shouldNotToBeNil() async throws {
         
-        let login = LoginInteractor()
+        try await loginInteractor?.sendLoginRequestWith(login: "romansamb", password: "Roman12345.")
         
-        try await login.sendLoginRequestWith(login: "romansamb", password: "Roman12345.")
-        
-        XCTAssertNotNil(login.newSession)
+        XCTAssertNotNil(loginInteractor?.newSession)
        
     }
     func test_LoginInteractor_createNewSession_shouldThrowAnError_incorrectUserNameOrPass() async throws {
         
-        let login = LoginInteractor()
-        
         do {
-            try await login.sendLoginRequestWith(login: "romansb", password: "Roman12345.")
+            try await loginInteractor?.sendLoginRequestWith(login: "romansb", password: "Roman12345.")
         } catch let error as AppError {
             XCTAssertEqual(error, AppError.incorrectUserNameOrPass)
         }
     }
     func test_LoginInteractor_fetchUserData_dataSuccessfullySaved() async throws {
         
-        let login = LoginInteractor()
-        
         let coreData = CoreDataManager.instance
-        let keyChain = KeyChainManager.instance
        
-        try await login.sendLoginRequestWith(login: "romansamb", password: "Roman12345.")
+        try await loginInteractor?.sendLoginRequestWith(login: "romansamb", password: "Roman12345.")
         
-        let sessionId = login.newSession?.session_id
+        let sessionId = loginInteractor?.newSession?.session_id
        
-        _ = try await login.fetchUserData(sessionId: sessionId!)
+        _ = try await loginInteractor?.fetchUserData(sessionId: sessionId!)
         
         let savedUser = try coreData.fetchUserDetails()
-        let accountID = keyChain.get(for: Constants.account_id)
+        let accountID = loginInteractor?.keychain.get(for: Constants.account_id)
         
         XCTAssertNotNil(accountID)
         XCTAssertEqual(accountID, String(savedUser?.id ?? 0))
