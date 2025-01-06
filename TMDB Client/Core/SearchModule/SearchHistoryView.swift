86ce58f240576d8490a1b0232731059d4c61ca39
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2025-01-06 15:48:05 +0100

    added search history view

---
+++ TMDB Client/Core/SearchModule/SearchHistoryView.swift
@@ -0,0 +1,27 @@
+//
+//  SearchHistoryView.swift
+//  TMDB Client
+//
+//  Created by Roman Samborskyi on 06.01.2025.
+//
+
+import UIKit
+
+class SearchHistoryView: UIView {
+    //MARK: - property
+    
+    //MARK: - lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("Fatal error: SearchHistoryView")
+    }
+}
+//MARK: - setup UI layout
+private extension SearchHistoryView {
+    func setupLayout() {
+        
+    }
+}
