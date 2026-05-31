cd493975ad4ee076d75fb2218f78e4b8f090768d
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2026-05-31 18:11:45 +0200

    added empty errro

--- TMDB Client/Core/AddtitionalViews/EmptyErrorView.swift
+++ TMDB Client/Core/AddtitionalViews/EmptyErrorView.swift
@@ -9,5 +9,40 @@ import UIKit
 
 class EmptyErrorView: UIView {
     //MARK: - properties
-    
+    private lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        return view
+    }()
+    private lazy var labelView: UILabel = {
+        let lbl = UILabel()
+        return lbl
+    }()
+    public var imageName: String = ""
+    var textLable: String = ""
+    //MARK: - life cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+//MARK: - setup layout
+private extension EmptyErrorView {
+    func setupLayout() {
+        self.backgroundColor = .customBackground
+        setupImageView()
+    }
+    func setupImageView() {
+        self.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "wifi.slash")
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+        ])
+    }
 }
